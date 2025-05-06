22b57deba7cc36bdcf12fdade626e3032be0561a
Sri Sreedhar <srisreedhar@live.com>
2025-05-06 14:06:43 +0530

    avg v2

---
+++ average_v2.py
@@ -0,0 +1,23 @@
+# wap that calculates the average of 6 subject marks
+
+# define 6 subject marks
+
+maths = input("Enter your maths marks     :")
+science = input("Enter your science marks     :")
+telugu = input("Enter your telugu marks     :")
+hindi = input("Enter your hindi marks     :")
+english = input("Enter your english marks     :")
+social = input("Enter your social marks     :")
+
+# Logic part
+
+
+# find the total of 6 subjects
+
+total = maths + telugu + hindi + social + science + english
+
+# divide the total with 6 
+
+avg = total/6
+
+print(avg)
