746639bfdb586dad2408fc1fbf510f7b3e27a4b5
Sri Sreedhar <srisreedhar@live.com>
2025-05-06 14:07:33 +0530

    avg v5

---
+++ average_v5.py
@@ -0,0 +1,28 @@
+# wap that calculates the average of 6 subject marks
+
+# define 6 subject marks
+
+maths = int(input("Enter your maths marks     :"))
+
+science = int(input("Enter your science marks     :"))
+
+telugu = int(input("Enter your telugu marks     :"))
+
+hindi = int(input("Enter your hindi marks     :"))
+
+english = int(input("Enter your english marks     :"))
+
+social = int(input("Enter your social marks     :"))
+
+
+# create new variables with modified values 
+
+
+
+total = maths+ telugu + hindi + social + science + english
+
+
+
+avg = total/6
+
+print(avg)
