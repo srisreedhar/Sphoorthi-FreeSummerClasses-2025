2bd3ed4e0c6252ee24903afbf62658c3a19f1ee1
Sri Sreedhar <srisreedhar@live.com>
2025-05-06 14:06:20 +0530

    Class workout

---
+++ typecasting-dir-len-strings-classworkout.py
@@ -0,0 +1,298 @@
+Python 3.11.7 (tags/v3.11.7:fa7a6f2, Dec  4 2023, 19:24:49) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+input()
+ssssssssssssss
+'ssssssssssssss'
+x = input()
+sssss
+
+x
+'sssss'
+x = input("--> ")
+--> 3333
+
+x
+'3333'
+
+
+x
+'3333'
+
+x =1111
+x
+1111
+
+input_a = 'somevalue'
+
+
+
+
+x
+1111
+
+x = input("--> ")
+--> 3333
+
+x
+'3333'
+
+type(x)
+<class 'str'>
+
+int(x)
+3333
+
+x
+'3333'
+
+# to change the initial value override
+
+x
+'3333'
+x = int(x)
+x
+3333
+
+x
+3333
+
+type(x)
+<class 'int'>
+
+
+
+x = str(x)
+x
+'3333'
+
+
+============= RESTART: D:/SphoorthiFreeClasses/python/average_v2.py ============
+Enter your maths marks     :80
+Enter your science marks     :80
+Enter your telugu marks     :80
+Enter your hindi marks     :80
+Enter your english marks     :80
+Enter your social marks     :80
+Traceback (most recent call last):
+  File "D:/SphoorthiFreeClasses/python/average_v2.py", line 21, in <module>
+    avg = total/6
+TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+maths
+'80'
+science
+'80'
+social
+'80'
+hindi
+'80'
+telugu
+'80'
+english
+'80'
+
+total
+'808080808080'
+
+avg
+Traceback (most recent call last):
+  File "<pyshell#56>", line 1, in <module>
+    avg
+NameError: name 'avg' is not defined
+
+primeminister
+Traceback (most recent call last):
+  File "<pyshell#58>", line 1, in <module>
+    primeminister
+NameError: name 'primeminister' is not defined
+
+apples
+Traceback (most recent call last):
+  File "<pyshell#60>", line 1, in <module>
+    apples
+NameError: name 'apples' is not defined
+
+============= RESTART: D:/SphoorthiFreeClasses/python/average_v3.py ============
+Enter your maths marks     :80
+Enter your science marks     :80
+Enter your telugu marks     :80
+Enter your hindi marks     :80
+Enter your english marks     :80
+Enter your social marks     :80
+80.0
+
+============= RESTART: D:/SphoorthiFreeClasses/python/average_v4.py ============
+Enter your maths marks     :70
+Enter your science marks     :70
+Enter your telugu marks     :70
+Enter your hindi marks     :70
+Enter your english marks     :70
+Enter your social marks     :70
+70.0
+
+
+maths
+70
+science
+70
+
+
+
+input()
+4444
+'4444'
+
+
+int(input())
+4444
+4444
+
+
+============= RESTART: D:/SphoorthiFreeClasses/python/average_v5.py ============
+Enter your maths marks     :50
+Enter your science marks     :50
+Enter your telugu marks     :50
+Enter your hindi marks     :50
+Enter your english marks     :50
+Enter your social marks     :50
+50.0
+
+============= RESTART: D:/SphoorthiFreeClasses/python/average_v5.py ============
+Enter your maths marks     :forty
+Traceback (most recent call last):
+  File "D:/SphoorthiFreeClasses/python/average_v5.py", line 5, in <module>
+    maths = int(input("Enter your maths marks     :"))
+ValueError: invalid literal for int() with base 10: 'forty'
+
+
+
+int("1234")
+1234
+
+int('five')
+Traceback (most recent call last):
+  File "<pyshell#78>", line 1, in <module>
+    int('five')
+ValueError: invalid literal for int() with base 10: 'five'
+
+
+
+
+name = "einstein"
+age = 72
+food = 'idly'
+
+print(name)
+einstein
+
+print(age)
+72
+
+print(name,age)
+einstein 72
+print(name,'is',age)
+einstein is 72
+
+
+print("His name is",name,"he is ",age,"years old","his fav food is",food)
+His name is einstein he is  72 years old his fav food is idly
+
+>>> 
+>>> 
+>>> 
+>>> "einstein"
+'einstein'
+>>> 
+>>> len("einstein")
+8
+>>> 
+>>> len(12345)
+Traceback (most recent call last):
+  File "<pyshell#103>", line 1, in <module>
+    len(12345)
+TypeError: object of type 'int' has no len()
+>>> 
+>>> 
+>>> len('12345')
+5
+>>> 
+>>> 
+>>> # uppercase
+>>> 
+>>> name = "einstein"
+
+name.upper()
+'EINSTEIN'
+
+name
+'einstein'
+
+name = name.upper()
+
+name
+'EINSTEIN'
+name
+'EINSTEIN'
+
+
+
+type(name)
+<class 'str'>
+
+
+len(name)
+8
+
+int('1')
+1
+
+
+name
+'EINSTEIN'
+
+name.lower()
+'einstein'
+
+
+
+dir(str)
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+
+
+name.capitalize()
+'Einstein'
+name
+'EINSTEIN'
+
+name.title()
+'Einstein'
+"what a great morning this is ".title()
+'What A Great Morning This Is '
+
+
+name
+'EINSTEIN'
+name.isupper()
+True
+
+
+"lowercase".isupper()
+False
+
+
+
+
+dir("sri")
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+
+
+
+dir(int)
+['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__', 'as_integer_ratio', 'bit_count', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes']
+
+dir(float)
+['__abs__', '__add__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getformat__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__int__', '__le__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__pos__', '__pow__', '__radd__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rmod__', '__rmul__', '__round__', '__rpow__', '__rsub__', '__rtruediv__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', 'as_integer_ratio', 'conjugate', 'fromhex', 'hex', 'imag', 'is_integer', 'real']
+
+
+dir()
+['__annotations__', '__builtins__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'age', 'food', 'name']
