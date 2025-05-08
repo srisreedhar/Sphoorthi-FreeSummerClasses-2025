2fc5be67d04ae7b073571bf29812277e96117bee
Sri Sreedhar <srisreedhar@live.com>
2025-05-08 12:03:22 +0530

    class workout

---
+++ if-condition-classworkout.py
@@ -0,0 +1,50 @@
+Python 3.11.7 (tags/v3.11.7:fa7a6f2, Dec  4 2023, 19:24:49) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> is_raining = True
+>>> 
+>>> if is_raining:
+...     print("carry umbrella")
+... 
+...     
+carry umbrella
+>>> 
+>>> 
+>>> is_raining= False
+>>> if is_raining:
+...     print("carry umbrella")
+... 
+...     
+>>> 
+>>> if True:
+...     print("this block is executed")
+... 
+...     
+this block is executed
+>>> 
+
+if 5>6:
+    print("5 is greater")
+else:
+    print("6 is greater")
+
+    
+6 is greater
+
+
+3/2
+1.5
+
+3%2
+1
+
+4%2
+0
+
+14%2
+0
+
+
+========== RESTART: D:/SphoorthiFreeClasses/python/if_even_odd_num.py ==========
+please enter a number : 5
+Entered number 5 is odd
