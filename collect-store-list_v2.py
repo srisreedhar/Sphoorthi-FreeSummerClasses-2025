c2f35c5c84b7afb6a88dedccd607309996337f0f
Sri Sreedhar <srisreedhar@live.com>
2025-05-12 20:38:59 +0530

    list append

---
+++ collect-store-list_v2.py
@@ -0,0 +1,31 @@
+# collect info from the user and store them in a data structure
+# collect 6 subject marks and store them in a list
+
+print("execution started")
+marks = []
+print("list created")
+maths = input("Enter your maths marks     :")
+print("math marks collected")
+marks.append(maths)
+print("math marks appened to marks list")
+science = input("Enter your science marks     :")
+print("science marks collected")
+marks.append(science)
+print("science marks appended to marks list")
+telugu = input("Enter your telugu marks     :")
+print("telugu marks created")
+marks.append(telugu)
+print("telugu marks appended to marks list")
+hindi = input("Enter your hindi marks     :")
+marks.append(hindi)
+english = input("Enter your english marks     :")
+marks.append(english)
+social = input("Enter your social marks     :")
+marks.append(social)
+
+print("the list of marks are",marks)
+
+
+
+
+
