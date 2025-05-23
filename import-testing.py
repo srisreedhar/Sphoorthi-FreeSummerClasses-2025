0340c70402bde356353b79d14dccd5f2eaaa77aa
Sri Sreedhar <srisreedhar@live.com>
2025-05-23 16:45:59 +0530

    imports

---
+++ import-testing.py
@@ -0,0 +1,20 @@
+import values
+
+# ModuleName.value
+# libraryName.value
+# FileName.value
+
+print(values.aadhar_num)
+print(values.address)
+
+print ("\n\n\n")
+
+print("marks =",values.marks)
+
+print ("\n\n")
+
+print(dir(values))
+
+# for number in values.numbers:
+#     if number%10 == 0:
+#         print("number is divisible by 10 :",number)
