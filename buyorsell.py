57be007adb317b3a27571effbe9148b6713f37de
Sri Sreedhar <srisreedhar@live.com>
2025-05-29 18:00:53 +0530

    assignment

---
+++ buyorsell.py
@@ -0,0 +1,4 @@
+# using the price in shareprice.py
+# write a program to buy or sell the shares
+# https://github.com/srisreedhar/Sphoorthi-FreeSummerClasses-2025/blob/main/shareprice.py
+# use imports and if conditions
